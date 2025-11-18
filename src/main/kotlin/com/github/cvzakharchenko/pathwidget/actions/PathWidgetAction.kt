6b2fd70720db3c02a107b1085b9da85ce0c1c72e
Kanstantsin Zakharchanka <k_zakharchanka@wargaming.net>
2025-11-18 23:08:31 +0100

    Fix but faster

--- src/main/kotlin/com/github/cvzakharchenko/pathwidget/actions/PathWidgetAction.kt
+++ src/main/kotlin/com/github/cvzakharchenko/pathwidget/actions/PathWidgetAction.kt
@@ -5,10 +5,15 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.ex.CustomComponentAction
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent
+import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.ui.components.JBLabel
+import com.intellij.util.messages.MessageBusConnection
 import com.intellij.util.ui.JBUI
 import javax.swing.JComponent
 
@@ -24,16 +29,63 @@ class PathWidgetAction : AnAction(), CustomComponentAction, DumbAware {
 
     override fun update(e: AnActionEvent) {
         val project = e.project
-        if (project == null) {
-            e.presentation.text = ""
-            e.presentation.description = null
+        e.presentation.putClientProperty(PROJECT_KEY, project)
+    }
+
+    override fun updateCustomComponent(component: JComponent, presentation: Presentation) {
+        if (component is PathLabel) {
+            val project = presentation.getClientProperty(PROJECT_KEY)
+            component.bindToProject(project)
+        }
+    }
+
+    companion object {
+        private val PROJECT_KEY = Key.create<Project>("PathWidgetAction.Project")
+    }
+}
+
+class PathLabel : JBLabel(), FileEditorManagerListener {
+    private var connection: MessageBusConnection? = null
+    private var currentProject: Project? = null
+
+    init {
+        border = JBUI.Borders.empty(0, 10)
+        text = ""
+    }
+
+    fun bindToProject(project: Project?) {
+        if (currentProject === project) {
             return
         }
 
+        connection?.disconnect()
+        connection = null
+        currentProject = null
+
+        if (project != null && !project.isDisposed) {
+            currentProject = project
+            connection = project.messageBus.connect()
+            connection?.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, this)
+            updatePath()
+        } else {
+            text = ""
+            toolTipText = null
+        }
+    }
+
+    override fun selectionChanged(event: FileEditorManagerEvent) {
+        updatePath()
+    }
+
+    private fun updatePath() {
+        val project = currentProject ?: return
+        if (project.isDisposed) return
+
         val file = FileEditorManager.getInstance(project).selectedFiles.firstOrNull()
+
         if (file == null) {
-            e.presentation.text = ""
-            e.presentation.description = null
+            text = ""
+            toolTipText = null
             return
         }
 
@@ -44,21 +96,14 @@ class PathWidgetAction : AnAction(), CustomComponentAction, DumbAware {
             file.path
         }
 
-        e.presentation.text = (relativePath ?: file.name).replace("/", " / ")
-        e.presentation.description = file.path
+        text = (relativePath ?: file.name).replace("/", " / ")
+        toolTipText = file.path
     }
 
-    override fun updateCustomComponent(component: JComponent, presentation: Presentation) {
-        if (component is PathLabel) {
-            component.text = presentation.text
-            component.toolTipText = presentation.description
-        }
-    }
-}
-
-class PathLabel : JBLabel() {
-    init {
-        border = JBUI.Borders.empty(0, 10)
-        text = ""
+    override fun removeNotify() {
+        super.removeNotify()
+        connection?.disconnect()
+        connection = null
+        currentProject = null
     }
 }
